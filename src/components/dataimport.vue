3b3b800c1f53c2d2dbb04cc7bd5e8fc4d5f36d1a
wangj <530405837@qq.com>
2019-08-30 15:16:57 +0800

    xiugai

--- src/components/dataimport.vue
+++ src/components/dataimport.vue
@@ -1,25 +1,97 @@
 <template>
-  <el-tabs type="border-card">
-    <el-tab-pane label="上传文件">
-        导入时间
-        <hr>
-        操作员
-        <hr>
-        任务领域
-        <hr>
-        备注信息
-        <hr>
-        <button>上传按钮</button>
-    </el-tab-pane>
-    <el-tab-pane label="数据库导入">
-        表名
-        <hr>
-        数据库类型
-        <hr>
-        连接字符串
-    </el-tab-pane>
-  </el-tabs>
+  <div class="dataimport">
+    <el-tabs>
+      <el-tab-pane label="上传文件">
+        <el-form ref="form" :model="form" label-width="100px">
+          <el-form-item label="导入时间">
+            <el-input v-model="form.time"></el-input>
+          </el-form-item>
+          <el-form-item label="操作员">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="任务领域">
+            <el-input v-model="form.areas"></el-input>
+          </el-form-item>
+          <el-form-item label="备注信息">
+            <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="form.information"></el-input>
+          </el-form-item>
+          <el-upload
+            class="upload-demo"
+            action="https://jsonplaceholder.typicode.com/posts/"
+            :on-preview="handlePreview"
+            :on-remove="handleRemove"
+            :before-remove="beforeRemove"
+            multiple
+            :limit="3"
+            :on-exceed="handleExceed"
+            :file-list="fileList"
+          >
+            <el-button size="small" type="primary">上传文件</el-button>
+          </el-upload>
+        </el-form>
+      </el-tab-pane>
+      <el-tab-pane label="数据库导入">
+        <el-form ref="form" :model="form" label-width="100px">
+          <el-form-item label="表名">
+            <el-input v-model="form.time"></el-input>
+          </el-form-item>
+          <el-form-item label="数据库类型">
+            <el-input v-model="form.name"></el-input>
+          </el-form-item>
+          <el-form-item label="连接字符串">
+            <el-input v-model="form.areas"></el-input>
+          </el-form-item>
+          <el-form-item label="备注信息">
+            <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="form.information"></el-input>
+          </el-form-item>
+        </el-form>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
 </template>
 
 <style scoped lang="less">
+.dataimport {
+  width: 800px;
+  margin: 0 auto;
+  .el-tabs {
+    margin-top: 0;
+  }
+}
 </style>
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        time: "",
+        name: "",
+        areas: "",
+        information: ""
+      },
+      fileList: []
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleExceed(files, fileList) {
+      this.$message.warning(
+        `当前限制选择 3 个文件，本次选择了 ${
+          files.length
+        } 个文件，共选择了 ${files.length + fileList.length} 个文件`
+      );
+    },
+    beforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`);
+    }
+  }
+};
+</script>
