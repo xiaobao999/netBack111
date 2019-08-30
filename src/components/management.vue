4b0de418c61111cc8e58ab95b3db62e8f149198d
wangj <530405837@qq.com>
2019-08-30 14:09:06 +0800

    元数据管理

--- src/components/management.vue
+++ src/components/management.vue
@@ -18,6 +18,30 @@
                 @click="close"
               ></el-button>
             </div>
+            <el-form ref="form" :model="form" label-width="80px">
+              <el-form-item label="ID">
+                <el-input v-model="form.id"></el-input>
+              </el-form-item>
+              <el-form-item label="标题">
+                <el-input v-model="form.title"></el-input>
+              </el-form-item>
+              <el-form-item label="作者">
+                <el-input v-model="form.author"></el-input>
+              </el-form-item>
+              <el-form-item label="年份">
+                <el-input v-model="form.year"></el-input>
+              </el-form-item>
+              <el-form-item label="机构">
+                <el-input v-model="form.mechanism"></el-input>
+              </el-form-item>
+              <el-form-item label="关键词">
+                <el-input v-model="form.keyword"></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" @click="onSubmit">保存</el-button>
+                <el-button @click="close">取消</el-button>
+              </el-form-item>
+            </el-form>
           </el-card>
         </div>
       </div>
@@ -53,20 +77,40 @@ export default {
   data() {
     return {
       input: "",
-      formshow: true,
+      formshow: false,
       tableData: [
         {
           id: "001",
           title: "标题1",
           author: "作者1",
           year: "2019",
-          mechanism: "",
-          keyword: ""
+          mechanism: "北京大学",
+          keyword: "很关键"
+        },
+        {
+          id: "002",
+          title: "标题1",
+          author: "作者1",
+          year: "2019",
+          mechanism: "北京大学",
+          keyword: "很关键"
         }
-      ]
+      ],
+      form: {
+        id: "",
+        title: "",
+        author: "",
+        year: "",
+        mechanism: "",
+        keyword: ""
+      }
     };
   },
   methods: {
+    onSubmit() {
+      console.log("submit!");
+      this.formshow = false;
+    },
     open() {
       this.formshow = true;
     },
@@ -95,9 +139,14 @@ export default {
 .management_main {
   width: 100%;
   height: 100%;
+  margin-top: 20px;
   display: flex;
   .form {
     width: 400px;
+    margin-right: 10px;
+    .el-card {
+      box-shadow: none;
+    }
   }
 }
 </style>
