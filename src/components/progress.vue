94c28e46b36eeb7cdc411757419c487324968d39
wangj <530405837@qq.com>
2019-09-03 15:50:51 +0800

    进度查看json读写

--- src/components/progress.vue
+++ src/components/progress.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="progress">
-    <el-button type="primary" plain>刷 新</el-button>
+    <el-button type="primary" plain @click="getdata">刷 新</el-button>
     <div class="progress_main">
+      <el-table :data="tableData" stripe style="width: 100%" border>
+        <el-table-column prop="date" label="Task ID"></el-table-column>
+        <el-table-column prop="name" label="进度条">
+          <template slot-scope="scope">
+            <el-progress :percentage="scope.row.name"></el-progress>
+          </template>
+        </el-table-column>
+        <el-table-column prop="state" label="工作状态"></el-table-column>
+        <el-table-column prop="mechanism" label="机构"></el-table-column>
+        <el-table-column fixed="right" label="操作">
+          <template slot-scope="scope">
+            <el-button type="text" size="small">启动</el-button>
+            <el-button type="text" size="small">停止</el-button>
+            <el-button type="text" size="small" @click="open(scope.row)">日志</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
       <div class="log" v-show="logshow">
         <div class="tittle">
           <el-card class="box-card">
@@ -14,27 +31,10 @@
                 @click="close"
               ></el-button>
             </div>
-            <div v-for="o in 10" :key="o" class="text item">{{'列表内容 ' + o }}</div>
+            <div v-for="(item,i) in progresslog" :key="i" class="text item">{{item}}</div>
           </el-card>
         </div>
       </div>
-      <el-table :data="tableData" stripe style="width: 100%" border>
-        <el-table-column prop="date" label="Task ID"></el-table-column>
-        <el-table-column prop="name" label="进度条">
-          <template slot-scope="scope">
-            <el-progress :percentage="scope.row.name"></el-progress>
-          </template>
-        </el-table-column>
-        <el-table-column prop="state" label="工作状态"></el-table-column>
-        <el-table-column prop="mechanism" label="机构"></el-table-column>
-        <el-table-column fixed="right" label="操作">
-          <template>
-            <el-button type="text" size="small">启动</el-button>
-            <el-button type="text" size="small">停止</el-button>
-            <el-button type="text" size="small" @click="open">日志</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
     </div>
   </div>
 </template>
@@ -43,41 +43,30 @@ export default {
   data() {
     return {
       logshow: false,
-      tableData: [
-        {
-          date: "52156321258",
-          name: 30,
-          state: "完好",
-          mechanism: "北京大学"
-        },
-        {
-          date: "52156321258",
-          name: 30,
-          state: "完好",
-          mechanism: "北京大学"
-        },
-        {
-          date: "52156321258",
-          name: 30,
-          state: "完好",
-          mechanism: "北京大学"
-        },
-        {
-          date: "52156321258",
-          name: 30,
-          state: "完好",
-          mechanism: "北京大学"
-        }
-      ]
+      tableData: [],
+      progresslog: []
     };
+  },
+  created() {
+    this.getdata();
   },
   methods: {
     handleClose() {},
-    open() {
+    async open(e) {
       this.logshow = true;
+      const id = e.id;
+      const res = await this.$http.get(`progresslog/${id}`);
+      //console.log(res);
+      this.progresslog = res.data.log;
+      //console.log(this.progresslog);
     },
     close() {
       this.logshow = false;
+    },
+    async getdata() {
+      console.log(JSON.stringify(this.progresslog));
+      const res = await this.$http.get(`progress`);
+      this.tableData = res.data;
     }
   }
 };
@@ -93,15 +82,23 @@ export default {
   display: flex;
   height: 100%;
   .log {
-    width: 400px;
-    margin-right: 10px;
-    .tittle{
+    flex: 1;
+    width: 100%;
+    margin-left: 10px;
+    .box-card {
+      width: 100%;
+    }
+    .tittle {
       height: 100%;
-      .el-card{
+      .el-card {
         height: 100%;
         box-shadow: none;
       }
     }
+  }
+  .el-table {
+    width: 100%;
+    flex: 4;
   }
 }
 </style>
