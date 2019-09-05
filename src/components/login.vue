29e32fa4e7b0b0907770ef497bd1fa4f66a9988e
wangj <530405837@qq.com>
2019-09-05 13:13:33 +0800

    登录

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,120 +1,61 @@
 <template>
-  <div class="login">
-    <el-card class="box-card">
-      <el-form
-        :model="ruleForm"
-        status-icon
-        :rules="rules"
-        ref="ruleForm"
-        label-width="100px"
-        class="demo-ruleForm"
-      >
-        <el-form-item label="年龄" prop="age">
-          <el-input v-model.number="ruleForm.age"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" prop="pass">
-          <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
-        </el-form-item>
-      </el-form>
-    </el-card>
+  <div class="login-wrap">
+    <el-form
+      :label-position="labelPosition"
+      label-width="80px"
+      :model="formdata"
+      class="login-form"
+    >
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button type="primary" class="login-btn" @click.prevent="handleLogin()">登陆</el-button>
+    </el-form>
   </div>
 </template>
-<style>
-.text {
-  font-size: 14px;
-}
-
-.item {
-  margin-bottom: 18px;
-}
-
-.clearfix:before,
-.clearfix:after {
-  display: table;
-  content: "";
-}
-.clearfix:after {
-  clear: both;
-}
-.login {
+<script>
+export default {
+  data() {
+    return {
+      labelPosition: "top",
+      formdata: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    async handleLogin() {
+      // const res = await this.$http.post(`login`, this.formdata);
+      // const {
+      //   data: {
+      //     data,
+      //     meta: { msg, status }
+      //   }
+      // } = res;
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.login-wrap {
+  //background: #324152;
   width: 100%;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
 }
-.box-card {
-  width: 480px;
+.login-form {
+  background-color: #ffffff;
+  border-radius: 5px;
+  width: 400px;
+  padding: 30px;
+}
+.login-btn {
+  width: 100%;
 }
 </style>
-<script>
-export default {
-  data() {
-    var checkAge = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error("年龄不能为空"));
-      }
-      setTimeout(() => {
-        if (!Number.isInteger(value)) {
-          callback(new Error("请输入数字值"));
-        } else {
-          if (value < 18) {
-            callback(new Error("必须年满18岁"));
-          } else {
-            callback();
-          }
-        }
-      }, 1000);
-    };
-    var validatePass = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请输入密码"));
-      } else {
-        if (this.ruleForm.checkPass !== "") {
-          this.$refs.ruleForm.validateField("checkPass");
-        }
-        callback();
-      }
-    };
-    var validatePass2 = (rule, value, callback) => {
-      if (value === "") {
-        callback(new Error("请再次输入密码"));
-      } else if (value !== this.ruleForm.pass) {
-        callback(new Error("两次输入密码不一致!"));
-      } else {
-        callback();
-      }
-    };
-    return {
-      ruleForm: {
-        pass: "",
-        checkPass: "",
-        age: ""
-      },
-      rules: {
-        pass: [{ validator: validatePass, trigger: "blur" }],
-        checkPass: [{ validator: validatePass2, trigger: "blur" }],
-        age: [{ validator: checkAge, trigger: "blur" }]
-      }
-    };
-  },
-  methods: {
-    submitForm(formName) {
-      this.$refs[formName].validate(valid => {
-        if (valid) {
-          alert("submit!");
-        } else {
-          console.log("error submit!!");
-          return false;
-        }
-      });
-    },
-    resetForm(formName) {
-      this.$refs[formName].resetFields();
-    }
-  }
-};
-</script>
